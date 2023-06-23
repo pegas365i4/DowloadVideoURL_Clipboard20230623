4305020fe4cb51d0a1813209a22aaa9ccc0c7df2
pegas365i4 <100947506+pegas365i4@users.noreply.github.com>
2023-06-24 00:44:28 +0300

    Add files via upload

---
+++ PreviewDownloadedVideos_20230624.bat
@@ -0,0 +1,5 @@
+@echo off
+REM https://github.com/pegas365i4/DowloadVideoURL_Clipboard20230624.git
+REM Запускает просмотр скаченныйх видео в папке DownloadVideos:
+mpv --playlist=DownloadVideos --loop-playlist
+
